20ec495148e4d6bc2696915acdc04126b16d23d7
Nathan Jorgensen <nj.jorg@gmail.com>
2026-03-26 14:02:15 +1000

    .

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ class SideBet:
 
 app = Flask(__name__)
 
-
-
 main_games = [
     Game(name='Blackjack', file_path='blackjack.html', etg='both'),
     Game(name='Blackjack Challenge', file_path='blackjack_challenge.html'),
@@ -26,39 +24,35 @@ main_games = [
     Game(name='Caribbean Stud Poker', file_path='caribbean_stud.html'),
     Game(name='Electronic Sic-Bo', file_path='electronic_sic-bo.html', etg='yes'),
     Game(name='Mississippi Stud Poker', file_path='mississippi_stud.html'),
-    Game(name='Roulette', file_path='roulette.html'),
+    Game(name='Roulette', file_path='roulette.html', etg='both'),
     Game(name='Sic-Bo', file_path='sic-bo.html'),
-
-
-    # {'name': 'Sic-Bo', 'file_path': 'sic-bo.html'},
-    # {'name': 'Soft 17 Blackjack', 'file_path': 'soft_17_blackjack.html'},
-    # {'name': 'Spanish Blackjack', 'file_path': 'spanish_blackjack.html'},
-    # {'name': 'Texas Hold\'em Bonus Poker', 'file_path': 'texas_holdem_bonus.html'},
-    # {'name': 'Three Card Poker', 'file_path': 'three_card_poker.html'},
-    # {'name': 'Ultimate Texas Hold\'em', 'file_path': 'ultimate_texas_holdem.html'},
-    # {'name': 'Wheel of Fortune', 'file_path': 'wheel_of_fortune.html'},
-    # {'name': 'Craps', 'file_path': 'craps.html'},
-    # {'name': 'Lucky Draw Baccarat', 'file_path': 'lucky_draw_baccarat.html'},
-    # {'name': 'Blackjack Switch', 'file_path': 'blackjack_switch.html'},
-    # {'name': 'Casino War', 'file_path': 'casino_war.html'},
+    Game(name='Soft 17 Blackjack', file_path='soft_17_blackjack.html'),
+    Game(name='Spanish Blackjack', file_path='spanish_blackjack.html'),
+    Game(name='Texas Hold\'em Bonus Poker', file_path='texas_holdem_bonus.html'),
+    Game(name='Three Card Poker', file_path='three_card_poker.html'),
+    Game(name='Ultimate Texas Hold\'em Poker', file_path='ultimate_texas_holdem.html'),
+    Game(name='Wheel of Fortune', file_path='wheel_of_fortune.html'),
+    Game(name='Craps', file_path='craps.html'),
+    Game(name='Lucky Draw Baccarat', file_path='lucky_draw_baccarat.html'),
+    Game(name='Blackjack Switch', file_path='blackjack_switch.html'),
+    Game(name='Casino War', file_path='casino_war.html'),
+    Game(name='RouletteX', file_path='roulettex.html', etg='yes'),
 ]
 
 side_bets = [
     SideBet(name='Perfect Pairs', file_path='sb_perfect_pairs.html', games=['baccarat', 'blackjack', 'blackjack_challenge', 'soft_17_blackjack'], etg='both'),
     SideBet(name='Lucky Lucky', file_path='sb_lucky_lucky.html', games=['blackjack', 'blackjack_challenge', 'soft_17_blackjack']),
-    # {'name': 'Perfect Pairs', 'file_path': 'sb_perfect_pairs.html', 'games': ['baccarat', 'blackjack', 'blackjack_challenge', 'soft_17_blackjack']},
-    # {'name': 'Dragon Bonus', 'file_path': 'sb_dragon_bonus.html', 'games': ['baccarat']},
-    # {'name': 'Tiger Baccarat', 'file_path': 'sb_tiger_baccarat.html', 'games': ['baccarat']},
-    # {'name': 'Perfect Pairs - Spanish', 'file_path': 'sb_perfect_pairs_spanish.html', 'games': ['spanish_blackjack']},
-    # {'name': 'Lucky Lucky', 'file_path': 'sb_lucky_lucky.html', 'games': ['blackjack', 'blackjack_challenge', 'soft_17_blackjack']},
-    # {'name': 'Table Jackpot System', 'file_path': 'sb_table_jackpot.html', 'games': ['mississippi_stud', 'caribbean_stud', 'texas_holdem_bonus', 'ultimate_texas_holdem', 'three_card_poker']},
-    # {'name': 'Player Pair or Banker Pair', 'file_path': 'sb_player_banker_pair.html', 'games': ['baccarat']},
-    # {'name': 'Super 6', 'file_path': 'sb_super_6.html', 'games': ['baccarat']},
-    # {'name': 'Super Sevens', 'file_path': 'sb_super_sevens.html', 'games': ['blackjack']},
-    # {'name': '3 Card Bonus', 'file_path': 'sb_three_card_bonus.html', 'games': ['mississippi_stud']},
-    # {'name': 'Star Pairs', 'file_path': 'sb_star_pairs.html', 'games': ['blackjack', 'blackjack_challenge', 'soft_17_blackjack', 'blackjack_switch']},
-    # {'name': 'Super Match', 'file_path': 'sb_super_match.html', 'games': ['blackjack_switch']},
-    # {'name': 'Tie Wager (Casino War)', 'file_path': 'sb_tie_wager.html', 'games': ['casino_war']},
+    SideBet(name='Dragon Bonus', file_path='sb_dragon_bonus.html', games=['baccarat']),
+    SideBet(name='Tiger Baccarat', file_path='sb_tiger_baccarat.html', games=['baccarat']),
+    SideBet(name='Perfect Pairs - Spanish', file_path='sb_perfect_pairs_spanish.html', games=['spanish_blackjack']),
+    SideBet(name='Table Jackpot System', file_path='sb_table_jackpot.html', games=['mississippi_stud', 'caribbean_stud', 'texas_holdem_bonus', 'ultimate_texas_holdem', 'three_card_poker']),
+    SideBet(name='Player Pair or Banker Pair', file_path='sb_player_banker_pair.html', games=['baccarat']),
+    SideBet(name='Super 6', file_path='sb_super_six.html', games=['baccarat']),
+    SideBet(name='Super Sevens', file_path='sb_super_sevens.html', games=['blackjack']),
+    SideBet(name='3 Card Bonus', file_path='sb_three_card_bonus.html', games='mississippi_stud'),
+    SideBet(name='Star Pairs', file_path='sb_star_pairs', games=['blackjack', 'blackjack_challenge', 'soft_17_blackjack', 'blackjack_switch']),
+    SideBet(name='Super Match', file_path='sb_super_match.html', games=['blackjack_switch']),
+    SideBet(name='Tie Wager (Casino War)', file_path='sb_tie_wager.html', games=['casino_war']),
 ]
 
 
